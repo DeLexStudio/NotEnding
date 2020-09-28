f82dcf0c86a4118b40b4b13e948c69a54a3e12d6
Alexandr <alexandrdemidov2002@gmail.com>
2020-09-28 11:11:21 +0300

    Добавьте файлы проекта.

---
+++ NotEnding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotEnding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ne = 0;
+            int sum = 0;
+            Console.WriteLine("Введите число p");
+            int p = Convert.ToInt32(Console.ReadLine());
+            while (sum < p)
+            {
+                Console.Write(ne + " ");
+                sum += ne;
+                ne++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
